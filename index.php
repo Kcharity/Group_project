b6dbefb17df1211f19f17b2fa3d9928d02ed115e
Kcharity <charitychiru@gmail.com>
2019-07-04 12:33:13 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'header.php';?>
+
+
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            <img src="food7.jpg" alt="">
+        </div>
+
+    </div>
+    </div>
+    <?php include 'footer.php';?>
